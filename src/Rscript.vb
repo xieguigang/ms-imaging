463b6960ee31ac64ede663324f8eaa4f16e990ac
xieguigang <xie.guigang@gcmodeller.org>
2021-10-12 19:46:11 +0800

    add api

--- src/Rscript.vb
+++ src/Rscript.vb
@@ -1,5 +1,13 @@
 
-<package("ggplot")>
+Imports ggplot
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("ggplot")>
 Public Module Rscript
 
+    <ExportAPI("geom_msimaging")>
+    Public Function geom_msimaging() As ggplotLayer
+
+    End Function
 End Module
