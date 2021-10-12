4adddc9485f843d1198c8863a4dd94c60499456a
xieguigang <xie.guigang@gcmodeller.org>
2021-10-12 19:50:18 +0800

    ms-imaging layer

--- src/Rscript.vb
+++ src/Rscript.vb
@@ -2,12 +2,21 @@
 Imports ggplot
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 <Package("ggplot")>
 Public Module Rscript
 
     <ExportAPI("geom_msimaging")>
-    Public Function geom_msimaging() As ggplotLayer
-
+    Public Function geom_msimaging(mz As Double) As ggplotLayer
+        Return New MSImagingLayer With {
+            .reader = New ggplotReader With {
+                .args = New list With {
+                    .slots = New Dictionary(Of String, Object) From {
+                        {"mz", mz}
+                    }
+                }
+            }
+        }
     End Function
 End Module
