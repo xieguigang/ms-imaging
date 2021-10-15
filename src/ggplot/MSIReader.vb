fe9a412698734509efa4ec47b58bfb89dc7701c5
xieguigang <xie.guigang@gcmodeller.org>
2021-10-15 15:12:12 +0800

    check arguments and implements the mzpack reader driver

--- src/ggplot/MSIReader.vb
+++ src/ggplot/MSIReader.vb
@@ -1,9 +1,24 @@
-﻿Imports ggplot
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
+Imports ggplot
 Imports SMRUCC.Rsharp.Runtime
 
 Public Class MSIReader : Inherits ggplotReader
 
+    ''' <summary>
+    ''' returns the dimensions of the MSI raw data
+    ''' </summary>
+    ''' <param name="data"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     Public Overrides Function getMapData(data As Object, env As Environment) As ggplotData
+        Dim raw As mzPack = DirectCast(data, mzPack)
+        Dim points = raw.MS.Select(Function(scan) scan.GetMSIPixel).ToArray
+        Dim x As Double() = points.Select(Function(p) CDbl(p.X)).ToArray
+        Dim y As Double() = points.Select(Function(p) CDbl(p.Y)).ToArray
 
+        Return New ggplotData With {
+            .x = x,
+            .y = y
+        }
     End Function
 End Class
--- src/ggplot/MSImagingLayer.vb
+++ src/ggplot/MSImagingLayer.vb
@@ -1,4 +1,5 @@
-﻿Imports ggplot
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports ggplot
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
 Imports Microsoft.VisualBasic.Imaging
@@ -14,6 +15,18 @@ Public Class MSImagingLayer : Inherits ggplotLayer
                                    scale As DataScaler,
                                    ggplot As ggplot.ggplot,
                                    theme As Theme) As legendGroupElement
+
+        Dim args = reader.args
+        Dim mz As Double = args.getValue(Of Double)("mz", ggplot.environment)
+        Dim mzdiff As Tolerance = args.getValue(Of Tolerance)("mzdiff", ggplot.environment)
+
+        If mz <= 0 Then
+            Throw New InvalidProgramException("invalid ion m/z value!")
+        End If
+        If mzdiff Is Nothing Then
+            mzdiff = Tolerance.DeltaMass(0.1)
+            ggplot.environment.AddMessage("missing 'tolerance' parameter, use the default da:0.1 as mzdiff tolerance value!")
+        End If
 
         Throw New NotImplementedException()
     End Function
