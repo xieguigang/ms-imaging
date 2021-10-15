d16d1b2aa95fcbae38448982f081f504ff5f6e40
xieguigang <xie.guigang@gcmodeller.org>
2021-10-16 00:26:47 +0800

    demo plot

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-SET R_HOME=D:\GCModeller\src\R-sharp\App\net5.0
+SET drive=%~d0
+SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net5.0
 
 %R_HOME%/Rscript.exe --build ./ /save ./MSImaging.zip
 %R_HOME%/R#.exe --install.packages ./MSImaging.zip
---
+++ docs/demo_MSI.R
@@ -0,0 +1,25 @@
+require(MSImaging);
+require(mzkit);
+require(ggplot);
+
+options(memory.load = "max");
+
+bitmap(file = `${@dir}/HR2MSI_mouse_urinary_bladder_S096.png`, size = [3000, 2400]) {
+    
+    # load mzpack/imzML raw data file
+    # and config ggplot data source driver 
+    # as MSImaging data reader
+    ggplot(open.mzpack(system.file("data/HR2MSI mouse urinary bladder S096 - Figure1.cdf", package = "MSImaging")), 
+           mapping = aes(driver = MSImaging()), 
+           padding = "padding: 200px 600px 200px 250px;"
+    ) 
+       # rendering of a single ion m/z
+       # default color palette is Jet color set
+       + geom_msimaging(mz = 741.5, tolerance = "da:0.3")
+       # add ggplot charting elements
+       + ggtitle("MSImaging of m/z 741.5")
+       + labs(x = "Dimension(X)", y = "Dimension(Y)")
+       + scale_x_continuous(labels = "F0")
+       + scale_y_continuous(labels = "F0")
+    ;
+}
