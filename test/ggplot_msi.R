87a2a0dd0f8b43962ddfd20b9b55699af0d2a904
xieguigang <xie.guigang@gcmodeller.org>
2021-10-15 17:01:59 +0800

    Update ggplot_msi.R

--- test/ggplot_msi.R
+++ test/ggplot_msi.R
@@ -1,19 +1,17 @@
-# require(MSImaging);
-
+require(MSImaging);
 require(mzkit);
 
 options(memory.load = "max");
 
-const raw = open.mzpack(system.file("data/S043_Processed_imzML1.1.1.mzPack", package = "MSImaging"));
-
 bitmap(file = `${@dir}/demo.png`, size = [3000, 2400]) {
-
-    ggplot(raw, aes(driver = MSImaging()), padding = "padding: 200px 600px 200px 250px;") 
-    + geom_msimaging(mz = 153.2, tolerance = "da:0.3")
-    + ggtitle("MSImaging of m/z 153.2")
-    + labs(x = "Dimension(X)", y = "Dimension(Y)")
-    + scale_x_continuous(labels = "F0")
-    + scale_y_continuous(labels = "F0")
+    ggplot(open.mzpack(system.file("data/S043_Processed_imzML1.1.1.mzPack", package = "MSImaging")), 
+           mapping = aes(driver = MSImaging()), 
+           padding = "padding: 200px 600px 200px 250px;"
+    ) 
+       + geom_msimaging(mz = 153.2, tolerance = "da:0.3")
+       + ggtitle("MSImaging of m/z 153.2")
+       + labs(x = "Dimension(X)", y = "Dimension(Y)")
+       + scale_x_continuous(labels = "F0")
+       + scale_y_continuous(labels = "F0")
     ;
-
 }
