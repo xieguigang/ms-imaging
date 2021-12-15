76f68df0e34f35054672a8376a6f468a591669ef
guigang xie <gg.xie@bionovogene.com>
2021-12-15 16:11:36 +0800

    build package

--- .pkg/build.cmd
+++ .pkg/build.cmd
@@ -3,7 +3,7 @@
 SET drive=%~d0
 SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net5.0
 
-%R_HOME%/Rscript.exe --build ./ /save ./MSImaging.zip
+%R_HOME%/Rscript.exe --build /src ../ /save ./MSImaging.zip --skip-src-build 
 %R_HOME%/R#.exe --install.packages ./MSImaging.zip
 
 pause
