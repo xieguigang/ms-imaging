b78eac9543bc8fc1e10e8ff7aae96365841c38a1
xieguigang <xie.guigang@gcmodeller.org>
2022-01-28 10:23:04 +0800

    auto scale size for ms-imaging plot

--- R/layout.R
+++ R/layout.R
@@ -1,3 +1,4 @@
+require(graphics2D);
 
 #' evaluate of the canvas size and padding
 #' 
@@ -8,17 +9,26 @@
 #' @param scale a numeric factor for describ the pixel scale 
 #'     size in MSImaging rendering process. default value is 1 
 #'     means no scale, just rendering of the origina size!
+#' @param padding  the padding element is a numeric vector with 
+#'     four elements standards for css padding value: 
+#'     ``[top, right, bottom, left]``. 
 #' 
 #' @return this function returns a list object that contains 
-#'   the recommended size value and canvas padding value for the 
-#'   MSImaging plot. the result value contains two slot elements:
-#'    
-#'    1. size     size element is a dimension vector with two 
-#'                elements standards for ``[w, h]``.
-#'    2. padding  the padding element is a numeric vector with 
-#'                four elements standards for css padding value: 
-#'                ``[top, right, bottom, left]``. 
+#'   the recommended size value for the MSImaging plot. the 
+#'   result value contains two slot elements: size element is 
+#'   a dimension vector with two elements standards for 
+#'   ``[w, h]``.
 #' 
-const autoSize as function(dims, scale = 1) {
+autoSize = function(dims, padding, scale = 1) {
+    dims    = graphics2D::sizeVector(dims);
+    padding = graphics2D::paddingVector(padding);
+    vpad    = padding[1] + padding[3];
+    hpad    = padding[2] + padding[4]; 
+    dims    = dims * scale;
+    dims    = [
+        dims[1] + hpad, # width
+        dims[2] + vpad  # height
+    ];
 
+    dims;
 }
