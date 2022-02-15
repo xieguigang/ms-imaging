3d9e8f5875444b2044e3f095609da408d1979728
guigang xie <gg.xie@bionovogene.com>
2022-02-15 10:14:50 +0800

    create MSI heatmap

--- src/ggplot/MSIHeatMap.vb
+++ src/ggplot/MSIHeatMap.vb
@@ -1,4 +1,5 @@
-﻿Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
+﻿Imports System.Drawing
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
 
 Public Class MSIHeatMap
 
@@ -6,4 +7,20 @@ Public Class MSIHeatMap
     Public Property G As SingleIonLayer
     Public Property B As SingleIonLayer
 
+    Public Property dimension As Size
+
+    Public Shared Function CreateLayer(layerName As String, pixels As Point(), heatmap As Double()) As SingleIonLayer
+        Return New SingleIonLayer With {
+            .IonMz = layerName,
+            .MSILayer = pixels _
+                .Select(Function(pt, i)
+                            Return New PixelData With {
+                                .intensity = heatmap(i),
+                                .x = pt.X,
+                                .y = pt.Y
+                            }
+                        End Function) _
+                .ToArray
+        }
+    End Function
 End Class
