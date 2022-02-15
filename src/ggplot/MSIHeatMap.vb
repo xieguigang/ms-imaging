55ca73f197bd4a6efd01e70a8c2ee28d54464dc5
guigang xie <gg.xie@bionovogene.com>
2022-02-15 10:01:44 +0800

    file renames

---
+++ src/ggplot/MSIHeatMap.vb
@@ -0,0 +1,9 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
+
+Public Class MSIHeatMap
+
+    Public Property R As SingleIonLayer
+    Public Property G As SingleIonLayer
+    Public Property B As SingleIonLayer
+
+End Class
