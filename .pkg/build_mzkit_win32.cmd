60e2fb25b7e5ebf86d05cdfe563a8aa94a3acb71
xieguigang <xie.guigang@gcmodeller.org>
2022-03-06 18:13:38 +0800

    Create build_mzkit_win32.cmd

---
+++ .pkg/build_mzkit_win32.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+SET mzkit_renv=../../../../dist/bin
+SET Rscript="%mzkit_renv%/Rscript.exe"
+SET REnv="%mzkit_renv%/R#.exe"
+
+SET mzkit_pkg=../../../../src/mzkit/setup/MSImaging.zip
+
+%Rscript% --build /src ../ /save %mzkit_pkg%
+%REnv% --install.packages %mzkit_pkg%
+
+pause
