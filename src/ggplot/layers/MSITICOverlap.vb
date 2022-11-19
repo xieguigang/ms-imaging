008c5bfc9aebbd94b633473cd0026d5f9399773c
xieguigang <xie.guigang@gcmodeller.org>
2022-11-20 00:49:40 +0800

    add two kind of plot element layer

---
+++ src/ggplot/layers/MSITICOverlap.vb
@@ -0,0 +1,13 @@
+﻿Imports ggplot
+Imports ggplot.elements.legend
+Imports ggplot.layers
+
+Namespace layers
+
+    Public Class MSITICOverlap : Inherits ggplotLayer
+
+        Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
