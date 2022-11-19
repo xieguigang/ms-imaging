e7f30c7d413216843f62969f19d63b36a0c1df66
xieguigang <xie.guigang@gcmodeller.org>
2022-11-20 01:01:44 +0800

    draw ruler

--- src/ggplot/layers/MSIRuler.vb
+++ src/ggplot/layers/MSIRuler.vb
@@ -1,13 +1,36 @@
-﻿Imports ggplot
+﻿Imports System.Drawing
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Reader
+Imports ggplot
 Imports ggplot.elements.legend
 Imports ggplot.layers
+Imports Microsoft.VisualBasic.MIME.Html.CSS
 
 Namespace layers
 
     Public Class MSIRuler : Inherits ggplotLayer
 
         Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
-            Throw New NotImplementedException()
+            Dim ggplot = stream.ggplot
+            Dim base = DirectCast(ggplot.base.reader, MSIReader)
+            Dim reader As PixelReader = base.reader
+            Dim resolution As Double = 17
+            Dim dimsize As Size = reader.dimension
+            Dim rect As Rectangle = stream.canvas.PlotRegion
+            Dim ratio As Double = rect.Width / dimsize.Width
+            Dim rulerWidth As Double = rect.Width * 0.2
+            Dim pen As Pen = Stroke.TryParse(stream.theme.lineStroke).GDIObject
+            Dim font As Font = CSSFont.TryParse(stream.theme.tagCSS).GDIObject(stream.g.Dpi)
+            Dim physical As String = (rulerWidth / ratio * resolution).ToString("F2") & " um"
+            Dim fontsize As SizeF = stream.g.MeasureString(physical, font)
+            Dim bottom As Double = rect.Bottom - fontsize.Height * 2
+            Dim left As New PointF(rect.Left + rect.Width * 0.05, bottom)
+            Dim right As New PointF(left.X + rulerWidth, bottom)
+            Dim center As New PointF(left.X + (rulerWidth - fontsize.Width) / 2, bottom + 10)
+
+            Call stream.g.DrawLine(pen, left, right)
+            Call stream.g.DrawString(physical, font, Brushes.Red, center)
+
+            Return Nothing
         End Function
     End Class
 End Namespace
