7be767162eb51d8c0a5ca0efb5d1f5d5b181448d
guigang xie <xie.guigang@gcmodeller.org>
2022-12-08 13:13:18 +0800

    draw spots

--- src/ggplot/layers/spatial/SpatialTranscriptomicsLayer.vb
+++ src/ggplot/layers/spatial/SpatialTranscriptomicsLayer.vb
@@ -1,7 +1,10 @@
+Imports System.Drawing
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.TissueMorphology
 Imports ggplot
 Imports ggplot.elements.legend
 Imports ggplot.layers
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Math2D
 Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 
 Namespace layers.spatial
@@ -15,7 +18,24 @@ Namespace layers.spatial
         Public Property ordinal As Integer
 
         Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
+            Dim ggplot As ggplotMSI = stream.ggplot
+            Dim dimension_size As Size = ggplot.GetDimensionSize(Nothing)
+            ' create a new transparent layer on current ms-imaging render layer
+            Dim layer As Graphics2D = dimension_size.CreateGDIDevice(filled:=Color.Transparent)
+            Dim rect As Rectangle = stream.canvas.PlotRegion
+            Dim fill As Brush = Brushes.Red
 
+            For Each spot As SpotMap In spots
+                Dim poly As New Polygon2D(spot.SMX, spot.SMY)
+                Dim data As Double = STdata.GetGeneExpression(spot.barcode)(ordinal)
+
+                Call layer.FillEllipse(fill, poly.GetRectangle)
+            Next
+
+            Call layer.Flush()
+            Call stream.g.DrawImage(layer.ImageResource, rect)
+
+            Return Nothing
         End Function
     End Class
 End Namespace
