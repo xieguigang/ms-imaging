f1ab2146c45d1416958f661edd143fed2c6b0d2d
xieguigang <xie.guigang@gcmodeller.org>
2023-01-15 22:33:57 +0800

    Update layout_test.R

--- test/layout_test.R
+++ test/layout_test.R
@@ -5,4 +5,4 @@ require(MSImaging);
                           # is_multiple_combine_wide = FALSE, 
                           # ratio_threshold = 1.5) {
 
-str(MSImaging::autoSize({w: 39, h: 339}, scale = 8, padding = "padding: 200px 600px 200px 250px;", ratio_threshold = 1.2));
+str(MSImaging::autoSize({w: 79, h: 367}, scale = 13, padding = "padding: 200px 600px 200px 250px;", ratio_threshold = 1.2));
