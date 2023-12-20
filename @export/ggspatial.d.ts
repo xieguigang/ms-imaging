bf29774ca3f22f3954fa95a59833ec51b59cafc0
guigang xie <xie.guigang@gcmodeller.org>
2023-12-20 10:43:41 +0800

    run package msbuild of the documents

--- @export/ggspatial.d.ts
+++ @export/ggspatial.d.ts
@@ -10,26 +10,42 @@
 */
 declare namespace ggspatial {
    /**
-     * @param size default value Is ``3``.
-     * @param alpha default value Is ``0.800000011920929``.
-     * @param env default value Is ``null``.
+    * draw scatter layer with a given x and y coordinates.
+    * 
+    * 
+     * @param x a numeric vector of pixel x
+     * @param y a numeric vector of pixel y
+     * @param colors the colors for rendering each scatter spot
+     * @param size the spot size for the drawing
+     * 
+     * + default value Is ``3``.
+     * @param alpha the transparency alpha value for drawing the scatter points.
+     * 
+     * + default value Is ``0.800000011920929``.
+     * @param env -
+     * 
+     * + default value Is ``null``.
    */
-   function geom_spatialScatter(x: any, y: any, colors: any, size?: number, alpha?: number, env?: object): any;
+   function geom_spatialScatter(x: any, y: any, colors: any, size?: number, alpha?: number, env?: object): object;
    /**
     * add a spatial overlaps of the STdata on current SMdata imaging
     * 
     * 
-     * @param tile -
-     * @param geneID 
+     * @param tile A collection of the spatial spot mapping
+     * @param geneID the gene ID for pull the spatial expression from the **`STdata`**.
+     * 
      * + default value Is ``null``.
-     * @param STdata 
+     * @param STdata the spatial transcriptomics data matrix
+     * 
      * + default value Is ``null``.
-     * @param colorSet 
+     * @param colorSet the color set data for rendering the gene expression spatial heatmap
+     * 
      * + default value Is ``'grays'``.
-     * @param size 
+     * @param size the spot size when drawing the spatial expression
+     * 
      * + default value Is ``13``.
      * @param env 
      * + default value Is ``null``.
    */
-   function geom_spatialtile(tile: object, geneID?: string, STdata?: any, colorSet?: any, size?: number, env?: object): any;
+   function geom_spatialtile(tile: object, geneID?: string, STdata?: any, colorSet?: any, size?: number, env?: object): object;
 }
--- man/MSImaging/ggspatial/ggspatial.md
+++ man/MSImaging/ggspatial/ggspatial.md
@@ -2,5 +2,5 @@
 
 spatial mapping between the STdata and SMdata based on the ggplot framework
 
-+ [geom_spatialScatter](ggspatial/geom_spatialScatter.1) 
++ [geom_spatialScatter](ggspatial/geom_spatialScatter.1) draw scatter layer with a given x and y coordinates.
 + [geom_spatialtile](ggspatial/geom_spatialtile.1) add a spatial overlaps of the STdata on current SMdata imaging
