5e6a60626e782b9ccc0cd2fb64fd6c6fc6889e04
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-02 09:04:22 +0800

    add xml dev notes

--- src/ggplot/data/PointPack.vb
+++ src/ggplot/data/PointPack.vb
@@ -5,16 +5,30 @@ Imports Microsoft.VisualBasic.Imaging.Math2D
 Namespace data
 
     ''' <summary>
-    ''' A collection of the pixel data 
+    ''' A collection of the <see cref="PixelData"/>
     ''' </summary>
     Public Class PointPack
 
+        ''' <summary>
+        ''' A collection of the <see cref="PixelData"/> for create heatmap rendering
+        ''' </summary>
+        ''' <returns></returns>
         Public Property pixels As PixelData()
         ''' <summary>
         ''' the canvas size of the ms-imaging
         ''' </summary>
         ''' <returns></returns>
         Public Property dimension As Size
+
+        ''' <summary>
+        ''' get the element number of the <see cref="PixelData"/> collection
+        ''' </summary>
+        ''' <returns>this property will returns ZERO if the array is null</returns>
+        Public ReadOnly Property size As Integer
+            Get
+                Return pixels.TryCount
+            End Get
+        End Property
 
         ''' <summary>
         ''' 这个函数会自动校准位置，尽量将目标多边形区域放置在中间
@@ -31,6 +45,12 @@ Namespace data
             Else
                 Return dimension
             End If
+        End Function
+
+        Public Overrides Function ToString() As String
+            With GetDimensionSize()
+                Return $"canvas(w={ .Width},h={ .Height}) contains {pixels.Length} spots"
+            End With
         End Function
 
     End Class
