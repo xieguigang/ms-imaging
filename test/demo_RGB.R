61fb691ba9a57300b3c86d87879feee156f1cf1a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-30 13:35:43 +0800

    svg test suiccess

---
+++ test/demo_RGB.R
@@ -0,0 +1,36 @@
+# imports "package_utils" from "devkit";
+
+setwd(@dir);
+
+# package_utils::attach("../../mzkit_app");
+# package_utils::attach("../../MSI_app"); 
+
+require(MSImaging);
+require(mzkit);
+require(ggplot);
+
+options(memory.load = "max");
+
+svg(file = `./HR2MSI_mouse_urinary_bladder_S096_RGB.svg`, size = [3300, 2000]) {
+    
+    # load mzpack/imzML raw data file
+    # and config ggplot data source driver 
+    # as MSImaging data reader
+    ggplot(open.mzpack(system.file("data/HR2MSI mouse urinary bladder S096 - Figure1.cdf", package = "MSImaging")),           
+           padding = "padding: 200px 600px 200px 250px;"
+    ) 
+       # rendering of rgb channels ion m/z
+       + geom_red(mz = 743.5468, tolerance = "da:0.3")
+       + geom_green(mz = 798.5414, tolerance = "da:0.3")
+       + geom_blue(mz = 741.5303, tolerance = "da:0.3")
+	   + theme(panel.background = "black", panel.grid = element_blank())
+	   + geom_MSIfilters(
+            TrIQ_scale(0.999)
+       )
+       # add ggplot charting elements
+       + ggtitle("HR2MSI mouse urinary bladder S096 - Figure1")
+       + labs(x = "Dimension(X)", y = "Dimension(Y)")
+       + scale_x_continuous(labels = "F0")
+       + scale_y_continuous(labels = "F0")
+    ;
+}
