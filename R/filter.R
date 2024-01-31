3afdf5911dda5ca9dcf7af811fe4f4fc208710a0
谢桂纲 <gg.xie@bionovogene.com>
2024-01-31 08:44:40 +0800

    adjust default filters

--- R/filter.R
+++ R/filter.R
@@ -2,6 +2,6 @@
 #'
 const default_MSIfilter = function() {
 	geom_MSIfilters(
-		denoise_scale() > TrIQ_scale(0.95) > knn_scale() > soften_scale()
+		denoise_scale() > TrIQ_scale(0.99) > knn_scale() > soften_scale()
 	);
 }
