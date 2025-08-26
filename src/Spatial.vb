3a2be0f75a4b87a455617b5e295d35371543fa5a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-08-26 15:19:44 +0800

    create test function

--- src/Spatial.vb
+++ src/Spatial.vb
@@ -1,63 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::46ec9d6baa7c3c57a7c9012e8ef2e054, Rscript\Library\MSI_app\src\Spatial.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 127
-    '    Code Lines: 85 (66.93%)
-    ' Comment Lines: 29 (22.83%)
-    '    - Xml Docs: 89.66%
-    ' 
-    '   Blank Lines: 13 (10.24%)
-    '     File Size: 5.36 KB
+' Summaries:
 
 
-    ' Module ggplotSpatial
-    ' 
-    '     Function: geom_spatialScatter, geom_spatialtile
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 127
+'    Code Lines: 85 (66.93%)
+' Comment Lines: 29 (22.83%)
+'    - Xml Docs: 89.66%
+' 
+'   Blank Lines: 13 (10.24%)
+'     File Size: 5.36 KB
+
+
+' Module ggplotSpatial
+' 
+'     Function: geom_spatialScatter, geom_spatialtile
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Blender
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.TissueMorphology
 Imports ggplot.colors
 Imports ggplotMSImaging.layers.spatial
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 Imports SMRUCC.Rsharp.Runtime
@@ -179,5 +181,18 @@ Public Module ggplotSpatial
             .colorMap = ggplotColorMap.CreateColorMap(colorSet, 1, env),
             .spotSize = size
         }
+    End Function
+
+    ''' <summary>
+    ''' test for the ruler formatter
+    ''' </summary>
+    ''' <param name="x">should be a numeric vector of ``μm`` for auto formatted</param>
+    ''' <returns></returns>
+    <ExportAPI("format_ruler")>
+    Public Function formatRuler(<RRawVectorArgument> x As Object) As Object
+        Return CLRVector.asNumeric(x) _
+            .SafeQuery _
+            .Select(AddressOf Ruler.AutoLengthFormat) _
+            .ToArray
     End Function
 End Module
